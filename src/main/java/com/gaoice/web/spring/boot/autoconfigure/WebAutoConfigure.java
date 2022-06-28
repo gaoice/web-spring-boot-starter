7e26ac66223788e29792d5e5067527db958e9590
GaoBing <gaoice@outlook.com>
2022-06-28 21:02:01 +0800

    feat: RedisTemplate bean

--- src/main/java/com/gaoice/web/spring/boot/autoconfigure/WebAutoConfigure.java
+++ src/main/java/com/gaoice/web/spring/boot/autoconfigure/WebAutoConfigure.java
@@ -4,11 +4,16 @@ import com.gaoice.web.spring.boot.autoconfigure.advice.WebExceptionAdvice;
 import com.gaoice.web.spring.boot.autoconfigure.advice.WebExceptionRAdvice;
 import com.gaoice.web.spring.boot.autoconfigure.advice.WebExceptionRTimeAdvice;
 import lombok.Data;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * @author gaoice
@@ -28,5 +33,25 @@ public class WebAutoConfigure {
             return new WebExceptionRTimeAdvice();
         }
         return new WebExceptionRAdvice();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnBean(RedisConnectionFactory.class)
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
+
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer();
+        redisTemplate.setValueSerializer(serializer);
+        redisTemplate.setHashValueSerializer(serializer);
+
+        redisTemplate.afterPropertiesSet();
+
+        return redisTemplate;
     }
 }
