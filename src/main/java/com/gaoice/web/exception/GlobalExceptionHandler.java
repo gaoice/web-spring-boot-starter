a6dd6e594c9fd8775a5487b5eba2b8c792c51896
gaoice <gaoice@qq.com>
2023-06-06 21:25:49 +0800

    feat: 新增全局异常日志记录

--- src/main/java/com/gaoice/web/exception/GlobalExceptionHandler.java
+++ src/main/java/com/gaoice/web/exception/GlobalExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.gaoice.web.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -13,9 +15,11 @@ public class GlobalExceptionHandler {
         ErrorResponse errorResponse = new ErrorResponse();
         if (ex instanceof WebException) {
             WebException webException = (WebException) ex;
+            log.debug("[WebException, code:{}, msg:{}]", webException.getCode(), webException.getMessage());
             errorResponse.setCode(webException.getCode());
             errorResponse.setMessage(webException.getMessage());
         } else {
+            log.error("[may be a unplanned exception]", ex);
             errorResponse.setCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
             errorResponse.setMessage(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
         }
